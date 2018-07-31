c23dc6357f726970d62dfd4f946a1e4a1eddd5f7
LizaCotter <ecotter20@allendalecolumbia.org>
2018-07-31 20:43:26 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -3,7 +3,11 @@ require_relative 'config/environment'
 class App < Sinatra::Base
 
   get '/' do 
-    "Welcome to your app!!!! "
+    "Welcome to your app!!!! Peyton is a real g. Hello "
+  end
+  
+  get '/nick' do
+    "what's up. Itsyoboi nick"
   end
 
 end
